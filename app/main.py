977614f620cf154334cfde6789221dab7ae7a6c1
jaruda-88 <jaruda88@gmail.com>
2024-01-30 20:24:20 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+def create_app():
+
+    app = FastAPI(debug=True)
+
+    return app
+
+
+app = create_app()
+
 
 @app.get('/')
 async def root():
     return {"message" : "Hello World"}
+
 
 @app.get('/items/{item_id}')
 def read_item(item_id : int, q : str | None = None):
