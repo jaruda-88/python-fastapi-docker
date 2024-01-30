be66d9e980ea4d5ab782ceb785e20ad2b786365e
jaruda-88 <jaruda88@gmail.com>
2024-01-30 20:46:03 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 def create_app():
 
     app = FastAPI(debug=True)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
 
